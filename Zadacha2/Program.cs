eec7d3bf715b56e7509f3195502a9e9ea3f8eefd
Egor <vova070393@mail.ru>
2023-01-31 14:00:25 +0300

    создал задачу два

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,34 @@
+﻿		Console.WriteLine("Введите по очереди три числа");
+		if(int.TryParse(Console.ReadLine(), out var firstNumber))
+		{
+			Console.WriteLine($"Ваше первое число {firstNumber}");
+		}
+		else
+		{
+			Console.WriteLine("Ввод не является целым числом");
+		}
+		if(int.TryParse(Console.ReadLine(), out var secondNumber))
+		{
+			Console.WriteLine($"Ваше второе число {secondNumber}");
+		}
+		else
+		{
+			Console.WriteLine("Ввод не является целым числом");
+		}
+        if(int.TryParse(Console.ReadLine(), out var thirdNumber))
+		{
+			Console.WriteLine($"Ваше третье число {thirdNumber}");
+		}
+		else
+		{
+			Console.WriteLine("Ввод не является целым числом");
+		}
+        int max = firstNumber;
+        
+		if (firstNumber > max) max = firstNumber;
+		if (secondNumber > max) max = secondNumber;
+        if (thirdNumber > max) max = thirdNumber;
+        
+		{
+			Console.WriteLine($"Большее число {max}");
+		}
